c5b9d7018da6f10ffc5fadb04056a096f85341b9
Adam Grygar <grygarad@gmail.com>
2023-03-19 22:15:38 +0000

    Add base ListView to voucher fragment (#11)

--- UserAppify/app/src/main/java/com/example/userappify/VoucherFragment.kt
+++ UserAppify/app/src/main/java/com/example/userappify/VoucherFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import com.example.userappify.databinding.FragmentVoucherBinding
 
 /**
@@ -12,6 +13,7 @@ import com.example.userappify.databinding.FragmentVoucherBinding
  */
 class VoucherFragment : Fragment() {
 
+    private var staticVouchers = arrayOf("1", "2")
     private var _binding: FragmentVoucherBinding? = null
 
     // This property is only valid between onCreateView and
@@ -22,9 +24,20 @@ class VoucherFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        _binding = FragmentVoucherBinding.inflate(inflater, container, false)
-        return binding.root
+        super.onCreateView(inflater, container, savedInstanceState);
+        val v = inflater.inflate(R.layout.fragment_voucher, container, false)
+        val listView = v.findViewById<ListView>(R.id.voucher_list_view)
+        listView.adapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, staticVouchers)
+        listView.onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, index, _ ->
+                Toast.makeText(
+                    context,
+                    "Clicked " + staticVouchers[index],
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        return v
 
     }
 
