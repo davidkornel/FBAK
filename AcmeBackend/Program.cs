efe209bc91c1ef2ff19b9d6ec1a12aa1681e651a
Benedek Lellei <lellei.benedek@gmail.com>
2023-03-28 11:25:47 +0100

    fix backend dto data types

--- AcmeBackend/Program.cs
+++ AcmeBackend/Program.cs
@@ -19,15 +19,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapPost("/register", ([FromBody] RegistrationRequestForm registrationRequestForm) => new RegistrationResponseForm("42", "43"))
+app.MapPost("/register", ([FromBody] RegistrationRequestForm registrationRequestForm) => new RegistrationResponseForm("cef0cbf3-6458-4f13-a418-ee4d7e7505d", "cef0cbf3-6458-4f13-a418-ee4d7e7505d"))
 .WithName("RegisterUser")
 .WithOpenApi();
 
-app.MapPost("/checkout", ([FromBody] CheckoutRequestForm checkoutRequestForm) => new CheckoutResponseForm(42))
+app.MapPost("/checkout", ([FromBody] CheckoutRequestForm checkoutRequestForm) => new CheckoutResponseForm(42.5))
 .WithName("CheckoutUser")
 .WithOpenApi();
 
-app.MapPost("/userdata", ([FromBody] UserDataRequestForm userDataRequestForm) => new UserDataResponseForm(new Transaction[] { new Transaction(new Product[] { new Product("42", 43) }, "42", "43", false) }, new int[] { 42 }))
+app.MapPost("/userdata", ([FromBody] UserDataRequestForm userDataRequestForm) => new UserDataResponseForm(new Transaction[] { new Transaction(new Product[] { new Product("42", 43) }, "42", "43", false) }, new string[] { "cef0cbf3-6458-4f13-a418-ee4d7e7505d" }))
 .WithName("FetchUserData")
 .WithOpenApi();
 
@@ -36,8 +36,8 @@ app.Run();
 internal record RegistrationRequestForm(string UserPublicKey);
 internal record RegistrationResponseForm(string UserId, string SuperMarketId);
 internal record CheckoutRequestForm(Product[] Products, string UserId, string? VoucherId, bool? DiscountNow, string Signature);
-internal record CheckoutResponseForm(int AmountPaid);
+internal record CheckoutResponseForm(double AmountPaid);
 internal record UserDataRequestForm(string UserId, string Signature);
-internal record UserDataResponseForm(Transaction[] LastTransactions, int[] AvailableVoucherIds);
+internal record UserDataResponseForm(Transaction[] LastTransactions, string[] AvailableVoucherIds);
 internal record Product(string Id, int Price);
 internal record Transaction(Product[] Products, string UserId, string? VoucherId, bool? DiscountNow);
