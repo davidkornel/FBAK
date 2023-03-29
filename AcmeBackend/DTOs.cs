fcdedf982f71cc07b634e010d3689e32c2717a6b
Benedek Lellei <lellei.benedek@gmail.com>
2023-03-30 00:14:37 +0100

    rearrange DTO member order

--- AcmeBackend/DTOs.cs
+++ AcmeBackend/DTOs.cs
@@ -1,11 +1,11 @@
 namespace AcmeBackend
 {
     public record RegistrationRequestForm(string UserPublicKey);
-    public record RegistrationResponseForm(string UserId, string SuperMarketPublicKey);
-    public record CheckoutRequestForm(Product[] Products, string UserId, string? VoucherId, bool? DiscountNow, string Signature);
+    public record RegistrationResponseForm(string SuperMarketPublicKey, string UserId);
+    public record CheckoutRequestForm(bool? DiscountNow, Product[] Products, string Signature, string UserId, string? VoucherId);
     public record CheckoutResponseForm(double AmountPaid);
-    public record UserDataRequestForm(string UserId, string Signature);
-    public record UserDataResponseForm(Transaction[] LastTransactions, string[] AvailableVoucherIds);
+    public record UserDataRequestForm(string Signature, string UserId);
+    public record UserDataResponseForm(string[] AvailableVoucherIds, Transaction[] LastTransactions);
     public record Product(string Id, int Price);
-    public record Transaction(Product[] Products, string UserId, string? VoucherId, bool? DiscountNow);
+    public record Transaction(bool? DiscountNow, Product[] Products, string UserId, string? VoucherId);
 }
