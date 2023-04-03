4e3690c3535cf92372773c662f96fa98eb48e263
Kornel David <konel1020@gmail.com>
2023-04-03 10:30:00 +0100

    Introduce ViewModel to share data between fragments

--- UserAppify/app/src/main/java/com/example/userappify/qrreader/QrReaderFragment.kt
+++ UserAppify/app/src/main/java/com/example/userappify/qrreader/QrReaderFragment.kt
@@ -2,17 +2,19 @@ package com.example.userappify.qrreader
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.example.userappify.R
 import com.example.userappify.databinding.FragmentQrReaderBinding
+import com.example.userappify.model.ProductHashViewModel
 import com.google.zxing.integration.android.IntentIntegrator
 import org.json.JSONException
-import org.json.JSONObject
-
 
 
 /**
@@ -27,19 +29,27 @@ class QrReaderFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private val viewModel: ProductHashViewModel by activityViewModels()
+
+    private lateinit var arrayAdapter: ArrayAdapter<String>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
         _binding = FragmentQrReaderBinding.inflate(inflater, container, false)
+        arrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, viewModel.products.value as List<String>)
+        binding.hashListView.adapter = arrayAdapter
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        viewModel.products.observe(viewLifecycleOwner, Observer { list ->
+            arrayAdapter.notifyDataSetChanged()
+        })
         setupScanner()
         setOnClickListener()
     }
@@ -73,13 +83,14 @@ class QrReaderFragment : Fragment() {
             } else {
                 // If QRCode contains data.
                 try {
-
+                    println(result.contents.toString())
+                    viewModel.addHashedProduct(result.contents.toString())
                     // Converting the data to json format
-                    val obj = JSONObject(result.contents)
+//                    val obj = JSONObject(result.contents)
 
                     // Show values in UI.
-                    binding.name.text = obj.getString("name")
-                    binding.siteName.text = obj.getString("site_name")
+//                    binding.name.text = obj.getString("name")
+//                    binding.siteName.text = obj.getString("site_name")
 
                 } catch (e: JSONException) {
                     e.printStackTrace()
