08167981662f3c312075ae44d70f36ac8d30d59e
Filippo Comastri <filippo.comastri2@studio.unibo.it>
2023-04-15 13:03:33 +0100

    small fixes

--- UserAppify/app/src/main/java/com/example/userappify/basket/CartFragment.kt
+++ UserAppify/app/src/main/java/com/example/userappify/basket/CartFragment.kt
@@ -2,7 +2,8 @@ package com.example.userappify.basket
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Base64
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,17 +12,17 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.ListView
 import android.widget.TextView
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.userappify.R
 import com.example.userappify.auth.AuthManager
-import com.example.userappify.deconding_utils.getPemCertificate
-import com.example.userappify.deconding_utils.getPubKey
 import com.example.userappify.deconding_utils.signContent
 import com.example.userappify.model.NamedProduct
 import com.example.userappify.model.ProductHashViewModel
 import com.google.gson.Gson
 import java.util.Base64.getEncoder
+
 
 class CartFragment : Fragment() {
 
@@ -37,23 +38,29 @@ class CartFragment : Fragment() {
         val v = inflater.inflate(R.layout.fragment_cart, container, false)
         val cbDiscount = v.findViewById<CheckBox>(R.id.cb_discount)
         cbDiscount.isChecked = false
+        cbDiscount.isEnabled = viewModel.getSelectedVoucher() != null
         val tvVoucher = v.findViewById<TextView>(R.id.tv_cart_voucher)
         tvVoucher.text = when (viewModel.getSelectedVoucher()) {
             null -> "No voucher selected"
             else -> "Voucher discount :"+ viewModel.getSelectedVoucher()?.discount.toString()+" %"
         }
+
         val listView = v.findViewById<ListView>(R.id.list_view_products)
         productAdapter = ProductAdapter(requireContext(), viewModel.products.value as ArrayList<NamedProduct>,viewModel)
         listView.adapter = productAdapter
-        val auth = activity?.let { AuthManager(it) }
-
-        val checkoutToSign = getCheckoutToSign(cbDiscount.isChecked, viewModel.products.value as ArrayList<NamedProduct>,
-            auth!!.getLoginUser()!!.id, viewModel.getSelectedVoucher()?.id,)
-
-        val signedContent = signContent(Gson().toJson(checkoutToSign).toString())
-        val dick = getEncoder().encodeToString(signedContent.toByteArray())
 
         v.findViewById<Button>(R.id.btn_checkout).setOnClickListener {
+
+            val auth = activity?.let { AuthManager(it) }
+
+            val checkoutToSign = getCheckoutToSign(
+                cbDiscount.isChecked, viewModel.products.value as ArrayList<NamedProduct>,
+                auth!!.getLoginUser()!!.id, viewModel.getSelectedVoucher()?.id,
+            )
+
+            val signedContent = signContent(Gson().toJson(checkoutToSign).toString())
+            val dick = getEncoder().encodeToString(signedContent.toByteArray())
+
             val checkout = getCheckoutFromNamedProducts(
                 viewModel.products.value as ArrayList<NamedProduct>,
                 viewModel.getSelectedVoucher()?.id,
@@ -64,7 +71,7 @@ class CartFragment : Fragment() {
 
             // encode as json
             val coJson = encode(checkout)
-            Log.d("DEBUG",coJson)
+            Log.d("coJson",coJson)
             // After the checkout the cart has to be empty
             viewModel.removeAllProd()
             productAdapter.notifyDataSetChanged()
